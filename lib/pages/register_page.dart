5c2454aaab0e563802ee8b7705da57fcc85cbe98
moman13 <35956421+moman13@users.noreply.github.com>
2023-07-28 12:44:03 +0300

    create : register page

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -8,11 +8,51 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  double? _deviceHeight, _deviceWidth;
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        color: Colors.red,
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: _deviceWidth! * 0.05,
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _titleWidget(),
+                _registerButton(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _titleWidget() {
+    return const Text(
+      "Finstagram",
+      style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+    );
+  }
+
+  Widget _registerButton() {
+    return MaterialButton(
+      onPressed: () {},
+      color: Colors.red,
+      child: Text(
+        'Register',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w400,
+        ),
       ),
     );
   }
