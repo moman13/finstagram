a249e277e1bbf78ba30dd60e35e5dc70cc19dd98
moman13 <35956421+moman13@users.noreply.github.com>
2023-07-28 19:38:05 +0300

    create : add image uploader

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -9,6 +12,9 @@ class RegisterPage extends StatefulWidget {
 
 class _RegisterPageState extends State<RegisterPage> {
   double? _deviceHeight, _deviceWidth;
+  final GlobalKey<FormState> _registerFormKey = GlobalKey<FormState>();
+  String? _name, _email, _password;
+  File? _image;
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
@@ -26,6 +32,8 @@ class _RegisterPageState extends State<RegisterPage> {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 _titleWidget(),
+                _profileImageWidget(),
+                _registrationForm(),
                 _registerButton(),
               ],
             ),
@@ -42,6 +50,66 @@ class _RegisterPageState extends State<RegisterPage> {
     );
   }
 
+  Widget _registrationForm() {
+    return Container(
+      height: _deviceHeight! * 0.30,
+      child: Form(
+        key: _registerFormKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _nameTextField(),
+            _emailTextField(),
+            _passwordTextField(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _nameTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(hintText: "Name"),
+      validator: (value) => value!.length > 0 ? null : "Please Enter Name",
+      onSaved: (value) {
+        setState(() {
+          _name = value;
+        });
+      },
+    );
+  }
+
+  Widget _emailTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(hintText: "Email.."),
+      onSaved: (_value) {
+        setState(() {
+          _email = _value;
+        });
+      },
+      validator: (_value) {
+        bool _result = _value!.contains(RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+"));
+        return _result ? null : "Please Enter email";
+      },
+    );
+  }
+
+  Widget _passwordTextField() {
+    return TextFormField(
+        obscureText: true,
+        decoration: const InputDecoration(hintText: "Password.."),
+        onSaved: (_value) {
+          setState(() {
+            _password = _value;
+          });
+        },
+        validator: (_value) =>
+            _value!.length > 6 ? null : "Please Enter Password ");
+  }
+
   Widget _registerButton() {
     return MaterialButton(
       onPressed: () {},
@@ -52,6 +120,31 @@ class _RegisterPageState extends State<RegisterPage> {
           color: Colors.white,
           fontSize: 20,
           fontWeight: FontWeight.w400,
+        ),
+      ),
+    );
+  }
+
+  Widget _profileImageWidget() {
+    var _imageProvider = _image != null
+        ? FileImage(_image!)
+        : const NetworkImage('https://i.pravatar.cc/300');
+    return GestureDetector(
+      onTap: () {
+        FilePicker.platform.pickFiles(type: FileType.image).then((_result) {
+          setState(() {
+            _image = File(_result!.files.first.path!);
+          });
+        });
+      },
+      child: Container(
+        height: _deviceHeight! * 0.15,
+        width: 200,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: _imageProvider as ImageProvider,
+          ),
         ),
       ),
     );
